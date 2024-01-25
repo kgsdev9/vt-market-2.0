b2bd0aa03059dd9457b6a3f21315320348155205
kgsdev8 <kgsdev8@gmail.com>
2024-01-25 22:34:56 +0100

    refactoring-routes

--- routes/web.php
+++ routes/web.php
@@ -34,63 +34,49 @@ use App\Http\Controllers\VendeurRegisterController;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+//user and vendors user et visiteurs routes
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/become-seller', [VendeurRegisterController::class, 'registerSeller'])->name('register.seller');
-Route::get('/sellers-testing-products', [VendeurRegisterController::class, 'index'])->name('gestion-testing');
-Route::get('/catalogue-product',[HomeController::class, 'catalogueProduct']);
-
-//authentification routes
-
-Route::get('/store/seller/vtp/{id}', [HomeController::class, 'boutiqueSeller'])->name('boutique.seller');
-Route::get('/annuaire-categories', CategoryController::class)->name('categorie.index');
-Route::get('/all/stores/vtp', [HomeController::class, 'allBoutique'])->name('boutique.index');
-
-//user profile
 Route::get('/espace-client', [DashboardUserController::class, 'customerDashboard'])->name('espace.client');
 Route::get('/mes-commandes', [DashboardUserController::class, 'orderCustomer'])->name('espace.orders');
 Route::get('/commande/{id}', [DashboardUserController::class, 'showOrder'])->name('show.orders');
 Route::get('/adresses-delivry', [DashboardUserController::class, 'adresseCustomer'])->name('adresse.client');
 Route::post('/registerAdresse', [AdresseController::class, 'store'])->name('register.adresse');
-//product-category-controllers
-
+Route::get('/become-seller', [VendeurRegisterController::class, 'registerSeller'])->name('register.seller');
+Route::get('/catalogue-product',[HomeController::class, 'catalogueProduct']);
+Route::get('/store/seller/vtp/{id}', [HomeController::class, 'boutiqueSeller'])->name('boutique.seller');
+Route::get('/annuaire-categories', CategoryController::class)->name('categorie.index');
+Route::get('/all/stores/vtp', [HomeController::class, 'allBoutique'])->name('boutique.index');
 Route::get('/category-product/{slug}', [HomeController::class ,'categoryProduct'])->name('product.categories');
 Route::get('/product/detail/{slug}', [HomeController::class, 'singleProduct'])->name('single.product');
-
-//mes routes  pour la connexion des des services de connexions par socialies
-
-Route::get("redirect/{provider}", [SocialiteConnecteControlller::class, 'redirect'])->name('socialite.redirect');
 Route::get('/product-management', ProductManagementController::class)->name('product.management');
 Route::get('/gestion-boutique', BoutiqueController::class)->name('gestion.boutique');
-//gestions du paniers et paiement
 Route::get('/cart', PanierController::class)->name('cart');
 Route::get('/sommary-oders', [HomeController::class, 'sommaryOrders'])->name('sommary.orders')->middleware('auth');
-Auth::routes();
-//
-Route::post('/resise', [HomeController::class, 'renitialiseImage'])->name('image.resize');
 
 
-//mes routes de notifications de paiement
-Route::get('/succes-payment', [PaymentNotificationController::class, 'successPayment']);
-Route::get('/echec-payment', [PaymentNotificationController::class, 'echecPayment']);
-
-
-//PaymentControllers
-Route::post('/initialise-payment-gateway', [PaymentController::class, 'initialisePayment'])->name('initialise.payment');
-
+//mes routes  pour la connexion des des services de connexions par socialies
+Route::get("redirect/{provider}", [SocialiteConnecteControlller::class, 'redirect'])->name('socialite.redirect');
 
 //notifications payement url
-
+Route::post('/initialise-payment-gateway', [PaymentController::class, 'initialisePayment'])->name('initialise.payment');
 Route::get('/success-payment', [PaymentNotificationController::class, 'successFullPayment'])->name('payment.sucess');
 Route::get('/failled-payment', [PaymentNotificationController::class, 'failedPayment'])->name('failled.payment');
 Route::get('/cancel-payment', [PaymentNotificationController::class, 'cancelPayment'])->name('cancel.payment');
 
 
 //administration gestion routes
+Route::middleware(['is_admin', 'auth'])->group(function () {
+    Route::get('/all/commandes', [AdminController::class, 'allCommandes'])->name('all.commandes');
+    Route::get('/detail/order/{id}', [AdminController::class, 'singleViewInvoice'])->name('view.order');
+    Route::get('/gestion-users', UserController::class)->name('geston.users');
+    Route::get('/all-boutique', ManagementBoutiqueController::class)->name('management.store');
+    Route::get('/role-management', RoleManagementController::class)->name('role.management');
+    Route::get('/city-management', CityManagementController::class)->name('city.management');
+    Route::get('/country-management', CountryManagementController::class)->name('country.management');
+});
 
-Route::get('/all/commandes', [AdminController::class, 'allCommandes'])->name('all.commandes');
-Route::get('/detail/order/{id}', [AdminController::class, 'singleViewInvoice'])->name('view.order');
-Route::get('/gestion-users', UserController::class)->name('geston.users');
-Route::get('/all-boutique', ManagementBoutiqueController::class)->name('management.store');
-Route::get('/role-management', RoleManagementController::class)->name('role.management');
-Route::get('/city-management', CityManagementController::class)->name('city.management');
-Route::get('/country-management', CountryManagementController::class)->name('country.management');
+
+
