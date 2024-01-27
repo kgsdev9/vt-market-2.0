b2c3d6bfa9566f09824904e9e576a5b7e909c1b0
kgsdev8 <kgsdev8@gmail.com>
2024-01-27 21:56:40 +0100

    refactoring-payment-notification-page

--- resources/views/notifications/Payments/sucess.blade.php
+++ resources/views/notifications/Payments/sucess.blade.php
@@ -7,11 +7,11 @@
             <div class="card-body">
                 <div class="mb-4">
                     <!-- heading -->
-                    <h2 class="mb-0">Echec du paiment </h2>
-                    <p>Parfois, des problèmes de connexion Internet peuvent entraîner des échecs de transactions en ligne. Assurez-vous d'avoir une connexion stable. </p>
-                            <a class="btn btn-outline-secondary"  href="{{route('cart')}}">Recommencer</a>
+                    <h2 class="mb-0">Transaction efféctuée avec success </h2>
+                    <p>Votre transaction a été effectué avec success vous allez recevoir dans quelque minute la facture de votre commande ainsi que les réferences. </p>
+                            <a class="btn btn-outline-secondary"  href="{{route('espace.orders')}}">Consulter vos commandes </a>
                 </div>
-                <svg width="64px" height="64px" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M6 12A6 6 0 106 0a6 6 0 000 12zM3 5a1 1 0 000 2h6a1 1 0 100-2H3z" fill="#000000"></path> </g></svg>
+                <svg width="64px" height="64px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <defs> <style>.cls-1{fill:none;stroke:#000000;stroke-linecap:round;stroke-linejoin:round;stroke-width:20px;}</style> </defs> <g data-name="Layer 2" id="Layer_2"> <g data-name="E408, Success, Media, media player, multimedia" id="E408_Success_Media_media_player_multimedia"> <circle class="cls-1" cx="256" cy="256" r="246"></circle> <polyline class="cls-1" points="115.54 268.77 200.67 353.9 396.46 158.1"></polyline> </g> </g> </g></svg>
             </div>
         </div>
 
